c06861827e17241f7c86b04b771a644d88f5a946
yaamanni kasavan <yaamanni19@gmail.com>
2022-08-27 14:42:24 +0800

    Created category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_032317) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_064116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
