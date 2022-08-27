25c0444a5ebb11f02a979075c913341de44d0c9b
yaamanni kasavan <yaamanni19@gmail.com>
2022-08-27 16:08:25 +0800

    updated category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_064116) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_065121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.bigint "category_id", null: false
+    t.string "image_url", default: "", null: false
+    t.string "video_url", default: "", null: false
+    t.integer "age_group", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_activities_on_category_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "type"
@@ -32,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_064116) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "categories"
 end
