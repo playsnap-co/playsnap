5a4a78d45c2cfdb53dee7e38532148aa86a9680a
yaamanni kasavan <yaamanni19@gmail.com>
2022-08-27 16:57:01 +0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'activities/new'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
