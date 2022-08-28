90ebfba6e908a162f64cc2e39a04a0ecd99efeb7
yaamanni kasavan <yaamanni19@gmail.com>
2022-08-28 15:44:46 +0800

    Have updated the file with activites and categories but have not done db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# puts 'Creating Categories...'
+# social = Category.new(type: "social")
+# social.save!
+
+# emotional = Category.new(type: "emotional")
+# emotional.save!
+
+# cognitive = Category.new(type: "cognitive")
+# cognitive.save!
+
+# physical = Category.new(type: "physical")
+# physical.save!
+
+# gross_motor = Category.new(type: "gross motor")
+# gross_motor.save!
+
+# puts 'Finished!'
+
+# puts 'Creating Activities...'
+# ping_pong_throw = Activity.new(title:"ping pong throw" description:"The goal is to get balls inside a box. We have been using our cardboard house for this purpose but any box would work. Items required: Ping pong balls" category:"gross motor" age_group:"1-2" image_url:"https://www.babyplayhacks.com/play-hacks/pong-cups" video_url:)
+# ping_pong_throw.save!
+
+# sorting_colours = Activity.new(title:"sorting colours" description:"Ask your toddler to sort items of the same colour bowl. Items required: Bowls" category:"gross motor" age_group:"2-3" image_url:"https://www.amazon.co.uk/EDX-Education-Coloured-Sorting-Bowls/dp/B07D6DZ7WS" video_url:)
+# sorting_colours.save!
+
+# puts 'Finished!'
