df7ff3e454387f1fed21092e96361ccb25b3dfb7
goh <99309242+goh0222@users.noreply.github.com>
2022-09-01 12:15:12 +0800

    About playsnap

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :design ]
+  skip_before_action :authenticate_user!, only: [ :home, :design, :about_playsnap ]
 
   def home
   end
 
   def design
+  end
+
+  def about_playsnap
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get "design", to: "pages#design"
+  get "about_playsnap", to: "pages#about_playsnap"
 
   # Defines the root path route ("/")
   # root "articles#index"
