fa47bb90ce89500ac830d3592e82eb448611aa82
goh <99309242+goh0222@users.noreply.github.com>
2022-09-07 19:25:44 +0800

    Add linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path    = require("path")
-const webpack = require("webpack")
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: "./app/javascript/application.js",
   },
   output: {
     filename: "[name].js",
@@ -14,7 +14,7 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+      maxChunks: 1,
+    }),
+  ],
+};
