c0f617ba9a725ffac4e50fc520adfa0e81dea961
goh <99309242+goh0222@users.noreply.github.com>
2022-09-14 20:38:34 +0800

    Add avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 
   has_many :children, dependent: :destroy
   has_many :wishlists
   has_many :activities, through: :wishlists
   has_many :hearts
   has_many :activities, through: :hearts
+
+  has_one_attached :photo
 end
